24b2b77a97d6bbf331eb948aacefe5a00b6da329
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-05-14 11:37:57 -0300

    asa

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("audio");
+    var link = document.querySelector(".img-link");
+
+    link.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que o link seja seguido
+
+        audio.play();
+
+        // Aguarda 1 segundo (1000 milissegundos) antes de seguir para o link
+        setTimeout(function() {
+            window.location.href = link.getAttribute("href");
+        }, 1500);
+    });
+});
+
